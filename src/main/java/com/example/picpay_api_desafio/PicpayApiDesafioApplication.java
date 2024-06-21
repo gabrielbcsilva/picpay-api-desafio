8e168170c4200fc0c512374e27f41b02336983ef
Gabriel Bruno <64500546+gabrielbcsilva@users.noreply.github.com>
2024-06-20 23:55:54 -0300

    first commit

---
+++ src/main/java/com/example/picpay_api_desafio/PicpayApiDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.example.picpay_api_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayApiDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/picpay_api_desafio/PicpayApiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picpay_api_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
