b7a485c1e6621b0e3d88a51d0d6f531242b371bf
Gabriel Bruno <64500546+gabrielbcsilva@users.noreply.github.com>
2024-09-17 23:28:06 -0300

    feat: Wallet Classes

---
+++ src/main/java/com/example/picpay/config/DataLoader.java
@@ -0,0 +1,14 @@
+package com.example.picpay.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/example/picpay/model/entity/WalletType.java
@@ -0,0 +1,46 @@
+package com.example.picpay.model.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_wallet_type")
+public class WalletType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public WalletType() {
+    }
+
+    public WalletType(Long id, String description) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+  public enum Enum{
+    USER(1L,"user"),
+    MERCHANT(2L,"merchant");
+
+    Enum(Long id, String description) {
+      this.id = id;
+      this.description = description;
+    }
+
+    private Long id;
+    private String description;
+    public WalletType get(){
+      return  new WalletType(id,description);
+    }
+  }
+}
