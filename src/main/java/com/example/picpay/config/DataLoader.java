958131f18473996041d9c37cbab2220a9a2093c6
Gabriel Bruno <64500546+gabrielbcsilva@users.noreply.github.com>
2024-09-18 23:56:50 -0300

    feat: Wallettype Enuns Create on Start up of application

--- src/main/java/com/example/picpay/config/DataLoader.java
+++ src/main/java/com/example/picpay/config/DataLoader.java
@@ -1,14 +1,31 @@
 package com.example.picpay.config;
 
+import com.example.picpay.model.entity.WalletType;
+import com.example.picpay.repository.WalletTypeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Configuration
 public class DataLoader implements CommandLineRunner {
 
+    private final WalletTypeRepository walletTypeRepository;
+
+    public DataLoader(WalletTypeRepository walletTypeRepository) {
+        this.walletTypeRepository = walletTypeRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-
+        Arrays.stream(WalletType.Enum.values()).forEach(
+                walletType->{
+                    Optional<WalletType> foundWallet = walletTypeRepository.findByDescription(walletType.get().getDescription()) ;
+                    if(foundWallet.isEmpty()){
+                        walletTypeRepository.save(walletType.get());
+                    }
+                }
+        );
     }
 }
