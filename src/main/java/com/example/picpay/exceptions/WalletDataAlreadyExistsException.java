de8c1cab7ad50779a272e534ec9ffd1676d9d34c
Gabriel Bruno <64500546+gabrielbcsilva@users.noreply.github.com>
2024-09-21 00:48:28 -0300

    feat: Exception if Wallet Already Exists

---
+++ src/main/java/com/example/picpay/exceptions/WalletDataAlreadyExistsException.java
@@ -0,0 +1,21 @@
+package com.example.picpay.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class WalletDataAlreadyExistsException extends PicPayException{
+    private String detail;
+
+    public WalletDataAlreadyExistsException(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+       var pb =  ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+       pb.setTitle("Wallet data already exists");
+       pb.setDetail(detail);
+       return pb;
+
+    }
+}
